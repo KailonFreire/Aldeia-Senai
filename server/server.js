a3f706689427405525b7af3e2f4d58ef90e3f236
KailonFreire <kscarregosa@gmail.com>
2023-06-25 13:12:17 -0300

    server vercel: erro resolvido

--- server/server.js
+++ server/server.js
@@ -133,5 +133,5 @@ app.post('/api/register', async (req, res) => {
   
 
 
-app.listen("aldeia-senai-server.vercel.app", () => console.log('Server is running on port https://aldeia-senai-server.vercel.app'));
+app.listen(5000, () => console.log('Server is running on port http://localhost:5000'));
 
