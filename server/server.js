d0470dfed283cee41efc3d2104eacc02eb5aca8c
KailonFreire <kscarregosa@gmail.com>
2023-06-25 13:02:26 -0300

    atualizando para server vercel

--- server/server.js
+++ server/server.js
@@ -133,5 +133,5 @@ app.post('/api/register', async (req, res) => {
   
 
 
-app.listen(5000, () => console.log('Server is running on port http://localhost:5000'));
+app.listen("aldeia-senai-server.vercel.app", () => console.log('Server is running on port https://aldeia-senai-server.vercel.app'));
 
