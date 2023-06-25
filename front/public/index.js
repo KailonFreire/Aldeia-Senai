37e392213f3e415aaea411da52da616638db3c09
KailonFreire <kscarregosa@gmail.com>
2023-06-25 13:40:10 -0300

    Erro resolvido: Conexão do front com o serververcel

--- front/public/index.js
+++ front/public/index.js
@@ -23,7 +23,7 @@ window.onload = () => {
   //   const language = document.getElementById('language').value
   //   const resultado = document.getElementById('resultado')
 
-  //   fetch('http://aldeia-senai-server.vercel.app:5000', {
+  //   fetch('https://aldeia-senai-server.vercel.app:5000', {
   //     method: 'POST',
   //     headers: {
   //       'Content-type': 'application/json'
