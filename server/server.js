c8d128d26de4eea6cb5263c934fca46ff7a028cb
KailonFreire <kscarregosa@gmail.com>
2023-06-25 15:13:37 -0300

    Correção da permissão cors

--- server/server.js
+++ server/server.js
@@ -21,12 +21,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// app.use((req, res, next) => {
-//   res.setHeader('Access-Control-Allow-Origin', 'https://aldeia-senai.vercel.app');
-//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-//   next();
-// });
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://aldeia-senai.vercel.app');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 
 app.get('/', async (req, res) => {
     res.status(200).send({
