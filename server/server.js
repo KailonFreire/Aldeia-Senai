e0f0c7de30c18ddf75cd7ffed8925e319d0b214e
KailonFreire <kscarregosa@gmail.com>
2023-06-25 13:56:57 -0300

    Permissão do cors concedida

--- server/server.js
+++ server/server.js
@@ -21,6 +21,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://aldeia-senai.vercel.app');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
 app.get('/', async (req, res) => {
     res.status(200).send({
         message: 'Hello from NewGPT',
